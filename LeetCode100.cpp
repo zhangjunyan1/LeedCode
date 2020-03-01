fca5696841fd739631a394d36a9347425c9cc038
zhangjunyan1 <1620123163@qq.com>
2020-03-01 12:32:35 +0800

    LeetCode100.cpp

---
+++ LeetCode100.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: LeetCode100.cpp
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: 2020年03月01日 星期日 12时30分06秒
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+   if(p == NULL && q == NULL)return true;
+    if(p == NULL || q == NULL)return false;
+    if(p->val - q->val)return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
